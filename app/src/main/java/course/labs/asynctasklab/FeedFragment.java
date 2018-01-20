1cb60cf72a9a4dbb5bf03a108f29991eb49e9aef
hp <namstudionsk@gmail.com>
2018-01-20 22:15:17 +0700

    Fix

--- app/src/main/java/course/labs/asynctasklab/FeedFragment.java
+++ app/src/main/java/course/labs/asynctasklab/FeedFragment.java
@@ -7,18 +7,20 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class FeedFragment extends Fragment {
+public class FeedFragment extends Fragment
+{
 
 	private TextView mTextView;
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
 		return inflater.inflate(R.layout.feed, container, false);
 	}
 
 	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
+	public void onActivityCreated(Bundle savedInstanceState)
+	{
 		super.onActivityCreated(savedInstanceState);
 		mTextView = (TextView) getView().findViewById(R.id.feed_view);
 		mTextView.setText(getArguments().getString(MainActivity.TAG_TWEET_DATA));
