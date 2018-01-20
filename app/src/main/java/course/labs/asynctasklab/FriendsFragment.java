877c2e82f7df17b9df13a3e6034d30355d2707c8
hp <namstudionsk@gmail.com>
2018-01-20 22:14:42 +0700

    Fix

--- app/src/main/java/course/labs/asynctasklab/FriendsFragment.java
+++ app/src/main/java/course/labs/asynctasklab/FriendsFragment.java
@@ -8,34 +8,38 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class FriendsFragment extends ListFragment {
+public class FriendsFragment extends ListFragment
+{
 
 	// HostingActivity
 	private SelectionListener mCallback;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
-		setListAdapter(new ArrayAdapter<String>(getActivity(),
-				android.R.layout.simple_list_item_1, MainActivity.FRIENDS_NAMES));
+		setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, MainActivity.FRIENDS_NAMES));
 	}
 
 	@Override
-	public void onAttach(Activity activity) {
+	public void onAttach(Activity activity)
+	{
 		super.onAttach(activity);
 
 		// Make sure that the hosting Activity has implemented
 		// the callback interface. 
-		try {
+		try
+		{
 			mCallback = (SelectionListener) activity;
-		} catch (ClassCastException e) {
-			throw new ClassCastException(activity.toString()
-					+ " must implement SelectionListener");
+		} catch (ClassCastException e)
+		{
+			throw new ClassCastException(activity.toString() + " must implement SelectionListener");
 		}
 	}
 
 	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
+	public void onActivityCreated(Bundle savedInstanceState)
+	{
 		super.onActivityCreated(savedInstanceState);
 		
 		// Enable user interaction only if data is available
@@ -44,19 +48,25 @@ public class FriendsFragment extends ListFragment {
 	}
 
 	// Enable/disable user interaction 
-	void setAllowUserClicks(boolean allowUserInteraction) {
+	void setAllowUserClicks(boolean allowUserInteraction)
+	{
 		getListView().setEnabled(allowUserInteraction);
-		if (allowUserInteraction) {
+		if (allowUserInteraction)
+		{
 			getListView().setBackgroundColor(Color.WHITE);
-		} else {
+		}
+		else
+		{
 			getListView().setBackgroundColor(Color.DKGRAY);
 		}
 	}
 
 	@Override
-	public void onListItemClick(ListView l, View view, int position, long id) {
+	public void onListItemClick(ListView l, View view, int position, long id)
+	{
 		// Inform hosting Activity of user's selection
-		if (null != mCallback) {
+		if (null != mCallback)
+		{
 			mCallback.onItemSelected(position);
 		}
 	}
