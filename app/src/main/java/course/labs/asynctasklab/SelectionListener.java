1925e4acec5bbf67ea2ad8a05f3f6eb9e95acd70
hp <namstudionsk@gmail.com>
2018-01-21 23:38:07 +0700

    Fix

--- app/src/main/java/course/labs/asynctasklab/SelectionListener.java
+++ app/src/main/java/course/labs/asynctasklab/SelectionListener.java
@@ -2,6 +2,6 @@ package course.labs.asynctasklab;
 
 interface SelectionListener
 {
-	public void onItemSelected(int position);
+	void onItemSelected(int position);
 	public boolean canAllowUserClicks();
 }
